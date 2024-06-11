48140a6ad4072893fe2878946a59717aefc04db9
Bahman <ghasemi.bahman2014@gmail.com>
2024-06-11 12:02:56 +0330

    Nested Scroll

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +34,52 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LazyColumn(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentPadding = PaddingValues(vertical = 16.dp, horizontal = 8.dp)
+                    ) {
+                        item {
+                            RowItems()
+                        }
+                        nestedScroll()
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun RowItems() {
+    LazyRow(
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(space = 16.dp, alignment = Alignment.Start),
+        contentPadding = PaddingValues(horizontal = 8.dp)
+    ) {
+        items(10) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Color.Red)
+            )
+        }
+    }
+}
+
+fun LazyListScope.nestedScroll() {
+    items(100) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .background(Color.LightGray)
+                .padding(16.dp)
+        )
+        {
+            Text(text = "Item ${it + 1}")
         }
     }
 }
