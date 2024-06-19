d21292b44f69c1757d48d99126b27ef1a7569d4c
Bahman <ghasemi.bahman2014@gmail.com>
2024-06-19 18:26:47 +0330

    Navigation SafeArgs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -59,6 +60,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +30,51 @@ class MainActivity : ComponentActivity() {
                     Column(
                         Modifier
                             .fillMaxSize()
-                            .padding(innerPadding))
+                            .padding(innerPadding)
+                    )
                     {
+                        val navController = rememberNavController()
+                        NavHost(navController = navController, startDestination = ScreenA) {
+                            composable<ScreenA> {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Button(onClick = {
+                                        navController.navigate(
+                                            ScreenB(
+                                                name = null,
+                                                age = 32
+                                            )
+                                        )
+                                    }) {
+                                        Text(text = "Go to Next")
+                                    }
+                                }
+                            }
 
+                            composable<ScreenB> { entry ->
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    val args = entry.toRoute<ScreenB>()
+                                    Text(text = "${args.name}, ${args.age} years old.")
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
+@Serializable
+object ScreenA
+
+@Serializable
+data class ScreenB(
+    val name: String?,
+    val age: Int
+)
