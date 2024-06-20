cf53721e6853325e48c808d78b1f6e6516a921c6
Bahman <ghasemi.bahman2014@gmail.com>
2024-06-20 17:20:15 +0330

    Deep Link

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.jetpackcompose
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +32,42 @@ class MainActivity : ComponentActivity() {
                     Column(
                         Modifier
                             .fillMaxSize()
-                            .padding(innerPadding))
+                            .padding(innerPadding)
+                    )
                     {
-
+                        val navController = rememberNavController()
+                        NavHost(navController = navController, startDestination = "home") {
+                            composable("home") {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Button(onClick = {
+                                        navController.navigate("detail")
+                                    }) {
+                                        Text(text = "to Detail")
+                                    }
+                                }
+                            }
+                            composable(
+                                route = "detail",
+                                deepLinks = listOf(
+                                    navDeepLink {
+                                        uriPattern = "https://bahmanghasemi.ir/{id}"
+                                        action = Intent.ACTION_VIEW
+                                    }
+                                ),
+                                arguments = listOf(
+                                    navArgument("id") {
+                                        type = NavType.IntType
+                                        defaultValue = -1
+                                    }
+                                )
+                            ) { entry ->
+                                val id = entry.arguments?.getInt("id")
+                                Text(text = "enter id: $id")
+                            }
+                        }
                     }
                 }
             }
